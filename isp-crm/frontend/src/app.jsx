cc20fae003b5ff656b715006c0060bdd719fe65f
Ankit-Mathur01 <ankitzhoo@gmail.com>
2026-03-01 23:50:42 +0530

    Create app.jsx

---
+++ isp-crm/frontend/src/app.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>ISP CRM Frontend Live 🚀</h1>
+      <p>Frontend successfully deployed.</p>
+    </div>
+  );
+}
